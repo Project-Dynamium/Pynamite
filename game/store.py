b70e6740b3dc3eb8212757a172ade7531c5e3877
MiddleRed <2405653592@qq.com>
2022-08-02 19:41:59 +0800

    [GAME] Store search implemented.

--- game/store.py
+++ game/store.py
@@ -1,7 +1,13 @@
+from atexit import register
 from __init__ import *
 
 # Public
-async def get_store_set(skip,limit,ranked=False,unranked=False,rev_date=False):
+async def get_store_set(skip,limit,ranked=False,unranked=False,rev_date=False,search=""):
+    # Avoid reserved character conflicts
+    for i in range(0,32):
+        reserved = r'~!@#$%^&*()_+{}|:"<M>?`-=[]\;,./'
+        search = search.replace(reserved[i]," ")
+
     q = [
         {"$skip":skip},
         {"$limit":limit},
@@ -9,7 +15,17 @@ async def get_store_set(skip,limit,ranked=False,unranked=False,rev_date=False):
     if ranked:
         q.insert(0,{"$match":{"isRanked":True}})
     if unranked:
-        q.insert(0,{"$match":{"isRanked":True}})
+        q.insert(0,{"$match":{"isRanked":False}})
+    if search != "":
+        reg = {"$regex": search, "$options": "i"}
+        reg = {"$match":
+                {"$or": [
+                    {"musicTitle": reg },
+                    {"noter": {"username":reg}},
+                    {"composerName":reg},
+            ]}
+        }
+        q.insert(0,reg)
     if not rev_date:
         q.insert(0,{"$sort":{"_id":-1}})
 
@@ -24,7 +40,7 @@ async def get_set_chart(_id):
 
 async def get_chart_set(_id):
     doc = await db["Chart"].find_one({"_id":ObjectId(_id)})
-    return doc["set_id"] if doc != None else 404
+    return doc["set_id"] if doc != None else None
 
 
 # Private
